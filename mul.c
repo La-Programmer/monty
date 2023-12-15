9f79fab875a85d38b078445959ccae3dda560889
La-Programmer <justinoghenekomeebedi@gmail.com>
2023-12-15 13:40:58 +0000

    Bug fix to mul functionality

--- mul.c
+++ mul.c
@@ -14,7 +14,7 @@ void mul(stack_t **stack, unsigned int line_number)
 
 	if (((*stack) == NULL) || ((*stack)->next == NULL))
 	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	data = ((*stack)->next->n) * ((*stack)->n);
